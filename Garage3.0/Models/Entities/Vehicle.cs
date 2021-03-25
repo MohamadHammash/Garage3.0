66118214c1eb7d27ac88571ac29118d247020f2c
Mohamad Hammash <mohamad.hammash@hotmail.com>
2021-03-25 16:37:39 +0100

    ViewModels - view edited

--- Garage3.0/Models/Entities/Vehicle.cs
+++ Garage3.0/Models/Entities/Vehicle.cs
@@ -44,5 +44,5 @@ namespace Garage3._0.Models.Entities
     }
 }
 
-// [RegularExpression("([1-2][0|9])?[0-9]{2}[0-1][0-9][0-3][0-9][-]?[0-9]{4}", ErrorMessage = "Not a valid PN")]
+
 //[Remote("RegNumExists", "ParkedVehicles", AdditionalFields = "Id")]
---
+++ Garage3.0/Models/ViewModels/MembersViewModels/MembersAddViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Garage3._0.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage3._0.Models.ViewModels.MembersViewModels
+{
+    public class MembersAddViewModel
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Personnummer { get; set; }
+        public DateTime MbShipRegDate { get; set; }
+        public DateTime ProEndDate { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
+
+
+    }
+}
