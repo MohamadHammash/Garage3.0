7b714c7ba436138bd72372189608cc79968f1e35
Mamery <mamery480@gmail.com>
2021-03-25 10:49:44 +0100

    Vehicle Entity

--- Garage3.0/Models/Entities/Vehicle.cs
+++ Garage3.0/Models/Entities/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,31 @@ namespace Garage3._0.Models.Entities
     public class Vehicle
 
     {
+        public int VehicleID { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+
+        public int NumberOfwheeels { get; set; }
+
+        public int VehicleTypeId { get; set; }
+
+        public int MemberId { get; set; }
+
+        public DateTime ArrivalTime
+        {
+            get; set;
+
+        }
+
+
+
+
     }
 }
