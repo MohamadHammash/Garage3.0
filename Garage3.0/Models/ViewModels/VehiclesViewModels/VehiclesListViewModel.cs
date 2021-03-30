47f5578434b0e5f19de491141a87f0d7dba040ef
Mohamad Hammash <mohamad.hammash@hotmail.com>
2021-03-30 16:55:51 +0200

    Added parking time to vehicles list

--- Garage3.0/Models/ViewModels/VehiclesViewModels/VehiclesListViewModel.cs
+++ Garage3.0/Models/ViewModels/VehiclesViewModels/VehiclesListViewModel.cs
@@ -17,11 +17,18 @@ namespace Garage3._0.Models.ViewModels.VehiclesViewModels
         public VehicleType VehicleType { get; set; }
 
         [Display(Name = "Arrival Time")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MMMM-dd HH:mm}")]
         public DateTime ArrivalTime { get; set; }
         [Display(Name = "Owner")]
         public string MemberFullName { get; set; }
         public string Membership => IsPro ? "Pro" : "Normal";
         public bool IsPro { get; set; }
+        [DataType(DataType.Time)]
+        [Range(typeof(TimeSpan), "00:00", "12:00")]
+        [DisplayFormat(DataFormatString = @"{0:hh\:mm}")]
+        public TimeSpan ParkingTime => DateTime.Now - ArrivalTime;
+
+
 
 
     }
